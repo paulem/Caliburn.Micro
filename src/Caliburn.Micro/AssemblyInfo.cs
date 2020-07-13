eb67917e983fa9f3717e5228093e694c0bc8d997
Pavel Mikhaylov <pavel.um@gmail.com>
2020-07-14 01:16:14 +0300

    Add XML namespace

---
+++ src/Caliburn.Micro/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("https://github.com/paulem/caliburn-micro", "cal")]
+[assembly: XmlnsDefinition("https://github.com/paulem/caliburn-micro", "Caliburn.Micro")]
